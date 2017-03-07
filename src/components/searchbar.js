9908f908a16ac3bc551c0452c9f2d056febcb1ae
Jan <jan@HAL9000.fritz.box>
2017-03-07 09:32:37 +0100

    states on input

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -11,12 +11,28 @@ import React, { Component } from 'react';
 // class SearchBar extends React.Component //if component is not defined in import
 class SearchBar extends Component {
 
+  //constructor
+  constructor (props) {
+    super(props);  //make react.component accessible
+    //here i define the object in which states are saved in
+    this.state = { term : '' };
+  }
+
+
+
   //defines function in classes, see syntax is different from vanilla js
   render() {
 
     //renders needs to return jsx so here we go
-    return <input onChange={ event => console.log(event.target.value)}/>;
+    //return <input onChange={ event => console.log(event.target.value)}/>;
     //return <input onChange={this.onInputChange} /> // ES6 less syntax If we write like this, we need the onInputChange Function in class
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term : event.target.value }) } />
+        value of input : { this.state.term}
+      </div>
+
+    );
   }
 
   //event
