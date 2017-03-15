ba2cc187420b2ddec177ef9c16d540e004c5e0ad
Jan <jan@HAL9000.fritz.box>
2017-03-15 15:23:33 +0100

    foo

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -5,15 +5,25 @@ const VideoListItem = ({ video }) => {
   //const video = props.video;
   //but ES6 gievs us ({video}) and its idential
 
+
+  //for simplifying things ill use a lot of bootstrap here as suggested
     console.log(video);
+
+  const ImageURL = video.snippet.thumbnails.default.url;
+  const VideoName = video.snippet.title;
+
   return(
-     <li>
-      <ul className="list-group">
-        <li className="list-group-item">{ video.snippet.title }</li>
-        <li className="list-group-item">{ video.snippet.channelTitle }</li>
-        <li className="list-group-item">{ video.snippet.description }</li>
-      </ul>
-     </li>
+   <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object"  src={ ImageURL } />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{ VideoName }</div>
+        </div>
+      </div>
+    </li>
   )
 }
 
